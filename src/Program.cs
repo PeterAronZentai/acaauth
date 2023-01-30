b065a7f68c0bb6025e61993d580f361e0ac9822a
Andrii Kaplanovskyi <andrii.kaplanovskyi@dcslsoftware.com>
2023-01-30 12:23:52 +0000

    Authorization added

--- src/Program.cs
+++ src/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,21 +11,42 @@ var azureADconfig = builder.Configuration.GetSection("AzureAd");
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SVC1 API",
+        Version = "v1",
+        Description = "SVC1 API"
+    });
+
+    options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows()
+        {
+            Implicit = new OpenApiOAuthFlow()
+            {
+                AuthorizationUrl = new Uri($"https://login.microsoftonline.com/{azureADconfig.GetValue<string>("TenantId")}/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri($"https://login.microsoftonline.com/{azureADconfig.GetValue<string>("TenantId")}/oauth2/v2.0/token"),
+
+                Scopes = new Dictionary<string, string>()
+                {
+                    { $"api://{azureADconfig.GetValue<string>("ClientId")}/Global.API", "Client Manager API" }
+                }
+            }
+        }
+    });
+
+    options.OperationFilter<AuthorizeCheckOperationFilter>();
+});
 
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
         .AddMicrosoftIdentityWebApi(azureADconfig);
 
 var scopes = azureADconfig.GetSection("Scopes").Get<string[]>();
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("DefaultScope",
-        policy =>
-            policy
-            .RequireAuthenticatedUser()
-            .RequireScope(scopes));
-});
+builder.Services.AddAuthorization();
 
 builder.Services.AddRequiredScopeAuthorization();
 
@@ -79,3 +102,37 @@ record WeatherForecast(string Date, int TemperatureC, string? Summary)
     public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 }
 
+internal sealed class AuthorizeCheckOperationFilter : IOperationFilter
+{
+    private readonly IConfiguration _configuration;
+
+    public AuthorizeCheckOperationFilter(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        // Check for authorize attribute
+        var hasAuthorize = context.MethodInfo.DeclaringType!.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any() ||
+                            context.MethodInfo.GetCustomAttributes(true).OfType<AuthorizeAttribute>().Any();
+
+        if (!hasAuthorize) return;
+
+        operation.Responses.TryAdd("401", new OpenApiResponse { Description = "Unauthorized" });
+        operation.Responses.TryAdd("403", new OpenApiResponse { Description = "Forbidden" });
+
+        var oAuthScheme = new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+        };
+
+        operation.Security = new List<OpenApiSecurityRequirement>
+            {
+                new()
+                {
+                    [ oAuthScheme ] = new [] { _configuration.GetValue<string>("IdentityAudience") }
+                }
+            };
+    }
+}
